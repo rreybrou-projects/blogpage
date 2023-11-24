5595d2ed713ef059a5fac06e96f72ca7a7a81011
renodomino <reinout.reybrouck@outlook.be>
2023-11-24 16:31:35 +0100

    content import

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,37 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Les pieds dans la mare!',
+    description: `I made a website for a dance school in Abidjan (Ivory Coast) in html, css & js. 
+    Their mission is to educate children from the ghettos through modern dance.`,
+    imgSrc: '/static/images/feetintheair.jpg',
+    href: '/blog/volunteering/les-pieds-dans-la-mare',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Thesis: Antwerpia - America',
+    description: `The 16th century painter Simon Pereyns is specifically known for his career in Mexico where he 
+    co-produced multiple monumental altar pieces in Novohispanic tradition.`,
+    imgSrc: '/static/images/fish.jpg',
+    href: '/blog/arthistory/antwerpia-america',
+  },
+  {
+    title: 'Volunteering for PSBE',
+    description: 'Research shows an underestimated and beneficial value of psychedelic-assisted-therapy, succesfully treating certain mental health disorders. As a volunteer for the belgian branch of the Psychedelic Society I helped with social media posts and the planning of educational & academic events.', 
+    imgSrc: '/static/images/northern_lights.jpg',
+    href: '/blog/volunteering/psbe',
+  },
+  {
+    title: 'My blogpage! 👾 ',
+    description: `Yes, this right here! A coding challenge, portfolio and future proof personal blogpage.`,
+    imgSrc: '/static/images/retrocomputer.png',
+    href: '/blog/technology/blogpageapp',
   },
 ]
 
 export default projectsData
+
+
+
+
+
+
+
