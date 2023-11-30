2c18b2f204000e9572ad641ce7e5045ac51d195d
renodomino <reinout.reybrouck@outlook.be>
2023-11-30 18:37:34 +0100

    updates cv én banner test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'Cv' })
+export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'reinout_reybrouck') as Authors
